a1f54afce55531522ccdff21cc82efe60fba25a6
Le Minh Hieu <hieubeo0612@gmail.com>
2021-03-25 15:48:56 +0700

    Update Js

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 let p = document.getElementsByTagName('p')
 let h1 = document.getElementById('title')
-let yes = document.getElementById('yes')
-let no = document.getElementById('no')
-yes.value = 'Có'
-no.value = 'Không'
-no.addEventListener('click', swap)
-yes.addEventListener('click', heart)
-function swap() {
-    yes.id = 'no'
-    no.id = 'yes'
-    yes = document.getElementById('yes')
-    no = document.getElementById('no')
-    yes.value = 'Có'
-    no.value = 'Không'
-    console.log(no);
-}
-function heart() {
-    for (let x of p) {
-        x.className = 'animate__animated animate__fadeInUp'
+let btn1 = document.getElementById('btn-1');
+let btn2 = document.getElementById('btn-2');
+btn1.value = "Có";
+btn2.value = "Không";
+btn1.addEventListener("click", button1);
+btn2.addEventListener("click", button2);
+function button1() {
+    if (btn1.value == "Không") {
+        btn1.value = "Có"
+        btn2.value = "Không"
     }
-    h1.className='animate__animated animate__slideInDown'
-    h1.innerHTML='❤️Meeeee tooooo❤️'
-    yes.className = 'no-opacity'
-    no.className = 'no-opacity'
+    else {
+        for (let x of p) {
+            x.className = 'animate__animated animate__fadeInUp'
+        }
+        h1.className = 'animate__animated animate__slideInDown'
+        h1.innerHTML = '❤️Meeeee tooooo❤️'
+        btn1.className = 'no-opacity'
+        btn2.className = 'no-opacity'
+    }
 }
+function button2() {
+    if (btn2.value == "Không") {
+        btn1.value = "Không"
+        btn2.value = "Có"
+    }
+    else {
+        for (let x of p) {
+            x.className = 'animate__animated animate__fadeInUp'
+        }
+        h1.className = 'animate__animated animate__slideInDown'
+        h1.innerHTML = '❤️Meeeee tooooo❤️'
+        btn1.className = 'no-opacity'
+        btn2.className = 'no-opacity'
+    }
+}
+
